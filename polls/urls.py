af91b641c178d65d9a0b5ccb956b17c2b6ad919f
justzino <originalll94@gmail.com>
2020-04-04 19:11:20 +0900

    part4. generic view로 변경에 따른 urls 변경, 오타 수정

--- polls/urls.py
+++ polls/urls.py
@@ -6,9 +6,9 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     # the 'name' value as called by the {% url %} template tag
-    path('', views.index, name='index'),    # ex: /polls/
-    path('<int:pk>/', views.detail, name='detail'),    # ex: /polls/5/
-    path('<int:pk>/results/', views.results, name='results'),  # ex: /polls/5/results/
-    path('<int:pk>/vote/', views.vote, name='vote'),   # ex: /polls/5/vote/
+    path('', views.IndexView.as_view(), name='index'),    # ex: /polls/
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),    # ex: /polls/5/
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),  # ex: /polls/5/results/
+    path('<int:question_id>/vote/', views.vote, name='vote'),   # ex: /polls/5/vote/
 ]
 
